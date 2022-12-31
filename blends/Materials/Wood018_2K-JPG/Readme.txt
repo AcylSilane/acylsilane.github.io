d5b8c2792d7c69db444dc2b814c320d128c57a2c
James Dean <24254612+AcylSilane@users.noreply.github.com>
2022-12-31 16:05:50 -0500

    Delete old site contents

--- blends/Materials/Wood018_2K-JPG/Readme.txt
+++
@@ -1,2 +0,0 @@
-PBR wood texture obtained from CC0Textures.com
-https://cc0textures.com/view?id=Wood018
--- blends/gen_atoms.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-import ase.cluster
-
-particle = ase.cluster.Icosahedron("Cu", 3) # 55 atoms
-particle.write("logo_np.xyz")
