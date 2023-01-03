b9558ad3000552fb3020d34572d4f9fa0e8a568e
James Dean <24254612+AcylSilane@users.noreply.github.com>
2023-01-03 01:12:38 -0500

    wrap up first draft of site

--- scripts/include-components.js
+++ scripts/include-components.js
@@ -1,19 +1,45 @@
-async function includeHTML(filePath, elementID){
+async function includeHTML(filePath, elementID) {
   const response = await fetch(filePath);
   const html = await response.text();
   const element = document.getElementById(elementID);
   element.insertAdjacentHTML("beforeend", html);
+  return element;
 }
 
-async function includeSiteComponents(){
-  const includeFiles = [
-    ["/includes/nav.html", "insert-navbar"],
-    ["/includes/footer.html", "insert-footer"],
-    ["/includes/header.html", "insert-header"]
-  ];
-  for (const [filePath, divID] of includeFiles) {
-    await includeHTML(filePath, divID);
-  }
+// Nav goes into header
+// Header goes before the body
+// Footer goes after the body
+async function includeSiteComponents() {
+  const body = document.body;
+  const [header_html, footer_html, nav_html] = await Promise.all([
+    await fetch("/includes/header.html"),
+    await fetch("/includes/footer.html"),
+    await fetch("/includes/nav.html"),
+  ]);
+  body.insertAdjacentHTML("beforebegin", await header_html.text());
+
+  const header = document.getElementsByTagName("header")[0];
+  header.insertAdjacentHTML("beforeend", await nav_html.text());
+  body.insertAdjacentHTML("afterend", await footer_html.text());
+
+  insertLastModifiedDate(document.getElementsByTagName("footer")[0]);
 }
 
+async function insertLastModifiedDate(element) {
+  const lastModified = new Date(document.lastModified).toLocaleString("en-US", {
+    hour12: false,
+    timeZone: "America/New_York",
+    timeZoneName: "short",
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+  });
+
+  const lastModifiedHTML = `<p class="footerLastModified">Last modified ${lastModified}</p>`;
+
+  element.insertAdjacentHTML("beforeend", lastModifiedHTML);
+}
 includeSiteComponents();
